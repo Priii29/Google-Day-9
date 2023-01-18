7aa82be6ee33cb28536f67ba2b21409a016ad4d9
Priyanshi Ramani <priyanshi1.ramani@gmail.com>
2023-01-18 18:39:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("script running");
+
+let pictures = document.querySelectorAll(".card-image");
+
+let quiz = {
+  color: ["logical", "creative", "logical", "creative"],
+  vacation: ["extrovert", "introvert", "extrovert", "introvert"],
+  pizza: ["creative", "logical", "creative", "logical"],
+  house: ["introvert", "extrovert", "introvert", "extrovert"],
+  fruit: ["logical", "creative", "logical", "creative"],
+  activity: ["extrovert", "introvert", "extrovert", "introvert"],
+};
+
+let quizTaker = {
+  logical: 0,
+  creative: 0,
+  extrovert: 0,
+  introvert: 0,
+};
+
+pictures.forEach((picture) => {
+  picture.addEventListener("click", (e) => {
+    let choice = picture.id.split("-");
+    console.log(choice);
+
+    picture.classList.toggle("has-background-light");
+    picture.classList.toggle("has-background-warning");
+
+    let answer = quiz[choice[0]][choice[1]];
+    quizTaker[answer]++;
+
+    if (choice[0] === "activity") {
+      let resultID;
+
+      if (quizTaker.logical > quizTaker.creative) {
+        resultID = "#logical-";
+      } else {
+        resultID = "#creative-";
+      }
+
+      if (quizTaker.introvert > quizTaker.extrovert) {
+        resultID += "introvert";
+      } else {
+        resultID += "extrovert";
+      }
+
+      let result = document.querySelector(resultID);
+      console.log(result);
+      result.classList.remove("hidden");
+    }
+  });
+});
